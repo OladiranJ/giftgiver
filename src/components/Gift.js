806814278d278ea1bb9449e530a9dbe2d1eb6198
OladiranJ <diran@clowte.com>
2020-10-13 13:02:57 -0700

    gift component test and build

---
+++ src/components/Gift.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { Form, Button } from 'react-bootstrap'
+
+class Gift extends Component {
+    state = {
+        person: '',
+        present: ''
+    }
+
+    render() {
+        return (
+            <div>
+                <Form>
+                    <Form.Group>
+                        <Form.Label>Person</Form.Label>
+                        <Form.Control 
+                            className='input-person'
+                            onChange={event => this.setState({ person: event.target.value })}
+                        />
+                    </Form.Group>
+                </Form>
+            </div>
+        )
+    }
+}
+
+export default Gift
